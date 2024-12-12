c7d3a77aa5682cc5f4003b78d0fcba34eacf5748
ﾀﾅｶ ﾄﾓｱｷ <minazukijiro@gmail.com>
2024-12-12 10:12:56 +0900

    update

--- main.js
+++ main.js
@@ -1,75 +1,82 @@
-const Parser = require('rss-parser');
-const parser = new Parser();
+const FeedMe = require('feedme');
+const { Readable } = require( "stream" );
 
 const TurndownService = require('turndown');
 const turndownService = new TurndownService();
 
-const crypto = require('crypto');
-const fs = require('fs');
-const readline = require('readline');
 const util = require('util');
+const crypto = require('crypto');
+
+const f = async (url, tag) => {
+  if (url === '') return;
+  fetch(url)
+    .then(res => {
+      if (!res.ok) {
+        console.error(new Error(`status code ${res.status}`));
+        return;
+      }
+
+      const parser = new FeedMe(true);
+
+      parser.on('finish', () => {
+        const feed = parser.done();
+
+        let n = 0;
+        feed.items.forEach(item => {
+          let hash = crypto.createHash('sha1').update(item.title).digest('hex').slice(-10);
+          let filepath = 'content/posts/' + hash + '.md';
+          if (fs.existsSync(filepath)) return;
+
+          let pubdate;
+          if ('pubdate' in item)
+            pubdate = new Date(item.pubdate);
+          else if ('dc:date' in item)
+            pubdate = new Date(item['dc:date']);
+
+          let today = new Date();
+          let expirydate = new Date();
+          expirydate.setDate(today.getDate() + 3);
+
+          let description = typeof(item.description) === 'string' ? item.description : item.description.text;
+          let content = description === '' ? '' : turndownService.turndown(description);
+
+          let text = util.format('+++\ntitle = """%s"""\ndate = %s\nexpiryDate = %s\ntags = %s\n+++\n%s\n\n[[source]](%s)\n',
+                                 item.title, pubdate.toISOString(), expirydate.toISOString(), JSON.stringify(tag), content, item.link);
+
+          fs.writeFileSync(filepath, text, err => { if (err) { console.log(err); throw(err) } });
+
+          n++;
+        });
+
+        console.log(`${feed.title}: ${n}/${feed.items.length}`);
+      });
+
+      Readable.fromWeb(res.body).pipe(parser);
+    });
+}
+
+const fs = require('fs');
 
 (async () => {
-  const stream = fs.createReadStream('urls', 'utf8');
-  const reader = readline.createInterface({input: stream, console: false});
-
-  reader.on('line', async line => {
-    data = line.split(' ');
-    let url = data[0];
+  const text = fs.readFileSync('./urls');
+  const arr = text.toString().split('\n');
+  arr.forEach(async elem => {
+    let a = elem.split(' ');
+    let url = a[0];
     let tag = [];
-    for (let i = 1; i < data.length; i++) {
-      let t = data[i];
-      if (data[i].slice(0, 1) === '"') {
-        t = data[i].slice(1);
-        for (i = i + 1; data[i].slice(-1) !== '"' && i < data.length; i++) {
-          t += ' ' + data[i];
+    if (url !== '') {
+      for(let i = 1; i < a.length; i++) {
+        let t = a[i];
+        if (a[i].slice(0, 1) === '"') {
+          t = a[i].slice(1);
+          for (i = i + 1; a[i].slice(-1) !== '"' && i < a.liength; i++)
+            t += ' ' + a[i];
+          t += ' ' + a[i].slice(0, -1);
         }
-        t += ' ' + data[i].slice(0, -1);
+        tag.push(t);
       }
-      tag.push(t);
     }
 
-    let feed = await parser.parseURL(url);
-    let n = 0;
-
-    feed.items.forEach(item => {
-      let hash = crypto.createHash('sha1').update(item.title).digest('hex').slice(-10);
-      let filepath = 'content/posts/' + hash + '.md';
-
-      if (!fs.existsSync(filepath)) {
-        let title = feed.title;
-        if ('title' in item) title = item.title;
-
-        let dateStr = null;
-        if ('isoDate' in item) dateStr = item.isoDate;
-        else if ('pubDate' in item) date = item.pubDate;
-        else if ('date' in item) date = item.date;
-
-        let date;
-        if (dateStr == null) date = new Date();
-        else date = new Date(dateStr);
-
-        let today = new Date();
-        let expiryDate = new Date();
-        expiryDate.setDate(today.getDate() + 3);
-
-        let markdown = '';
-        if ('content' in item) markdown = turndownService.turndown(item.content);
-        else {
-          fetch(item.link).then((resp) => {
-            if (resp.ok)
-              markdown = turndownService.turndown(resp.text);
-          });
-        }
-
-        let text = util.format('+++\ntitle = """%s"""\ndate = %s\nexpiryDate = %s\ntags = %s\n+++\n%s\n\n[[source]](%s)\n',
-                               title, date.toISOString(), expiryDate.toISOString(), JSON.stringify(tag), markdown, item.link);
-        fs.writeFileSync(filepath, text, err => { if (err) { console.log(err); throw(err) } });
-
-        n++;
-      }
-    });
-
-    console.log(util.format('%s: new %d/%d items', feed.title, n, feed.items.length));
+    f(url, tag);
   });
 })();
